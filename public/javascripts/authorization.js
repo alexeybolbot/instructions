a5c75586235e84193e701289e7efd86b0c8dc1df
Alexey Bolbot <alexeybolbot@mail.ru>
2017-10-24 23:20:38 +0300

    regist

--- public/javascripts/authorization.js
+++ public/javascripts/authorization.js
@@ -14,5 +14,15 @@ angular.module('authorizationApp', [])
                 $http.post('session/rememberMe');
             window.location = "http://localhost:3000/"+$scope.link;
         };
+        
+        $scope.regist = function(){
+            var firstName = $('#inputFirstName').val();
+            var lastName = $('#inputLastName').val();
+            var email = $('#inputEmail').val();
+            var password = $('#inputPassword').val();
+            var password2 = $('#inputPassword2').val();
+            $('#modalRegist').modal('hide');
+            $http.post('users/regist',{familyName:firstName + " " + lastName, email:email, password:password});
+        };
        
     });
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,19 @@
 var express = require('express');
+var randomstring = require("randomstring");
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
+var mail = require('../public/json/mail');
 var connection = require('../public/javascripts/mysql');
 var con = connection.Connection;
+
+var transporter = nodemailer.createTransport({
+    service: mail.service,
+    auth: {
+        user: mail.user,
+        pass: mail.pass
+    }
+});
 
 router.post('/saveStyle', function(req,res){
     req.session.style = req.body.timesOfDay;
@@ -15,5 +26,46 @@ router.post('/saveStyle', function(req,res){
         req.session.save();
     res.send(req.session.style);
 });
+
+router.post('/regist', function(req,res){   
+    con.query('SELECT * FROM user WHERE email="'+req.body.email+'"', function(err, result) {
+        if(result.length == 0)
+            sendEmail(req);
+        else 
+            res.send("passwordIsBusy");
+    });
+});
+
+function sendEmail(req){
+    var secretToken = randomstring.generate() + req.body.email;
+    var url = "http://localhost:3000/verification/"+secretToken;
+
+    var mailOptions = {
+        from: mail.user,
+        to: req.body.email,
+        subject: 'Подтверждение регистрации',
+        html: '<h1>Перейдите по ссылке, для завершения регистрации.</h1>\n\
+              <p><a href="'+url+'">Подтвердить!!!</a></p>'
+    };
+
+    var obj = req.body;
+    obj.status = 0;
+    obj.secretToken  = secretToken;
+    
+    con.query('INSERT INTO user SET ?', obj, function(err, result) {});
+
+    transporterSendMail(mailOptions);
+        
+};
+
+function transporterSendMail(mailOptions){
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });    
+};
 
 module.exports = router;
